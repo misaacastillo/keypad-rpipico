3b599382ba61068752958d6b50a0398eb6a561f4
Misael Castillo <102986218+misaacastillo@users.noreply.github.com>
2024-09-13 23:36:29 -0300

    Update keypad.h

--- include/keypad.h
+++ include/keypad.h
@@ -1,13 +1,17 @@
-#ifndef PICO_KEYPAD4X4_H
-#define PICO_KEYPAD4X4_H
+/*
+ * Copyright (c) 2024 Misael Castillo <misacastillo08@gmail.com>
+*/
+
+#ifndef _KEYPAD_H_
+#define _KEYPAD_H_
 
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
+// prototipos de funciones
+
 void pico_keypad_init(uint columns[4], uint rows[4], char matrix_values[16]);
-
 char pico_keypad_get_key(void);
-
 void pico_keypad_irq_enable(bool enable, gpio_irq_callback_t callback);
 
 #endif
